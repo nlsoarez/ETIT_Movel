b9a2fb81ca71405bda48bc436945882fb4e8f602
nlsoarez <nelson.soares@claro.com.br>
2025-03-08 03:52:54 -0300

    Delete upload.php

--- upload.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_FILES['file']['name']) {
-    $filename = $_FILES['file']['tmp_name'];
-    move_uploaded_file($filename, 'data.xlsx');
-    echo json_encode(['success' => true]);
-} else {
-    echo json_encode(['success' => false]);
-}
-?>
