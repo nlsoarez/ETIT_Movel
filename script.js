4d7d92813fff52ea210ae0455928cc431d3aa5a0
nlsoarez <nelson.soares@claro.com.br>
2025-03-12 02:52:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("btnAjuda").classList.add("hidden");
+    document.getElementById("btnAjuda")?.classList.add("hidden");
+
+    // Carregar última matrícula consultada
+    const ultimoLogin = localStorage.getItem("ultimoLogin");
+    if (ultimoLogin) {
+        document.getElementById("login").value = ultimoLogin;
+    }
+
+    // Permitir consulta ao pressionar "Enter"
+    document.getElementById("login").addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            buscarDados();
+        }
+    });
 });
 
 function buscarDados() {
@@ -13,6 +26,7 @@ function buscarDados() {
         return;
     }
 
+    // Simulação de banco de dados
     let dados = {
         "N6071740": { "Abertura RAL": 0, "RAL": 0, "REC": 0, "Remedy Móvel": 418, "TOA: 1ª Int.": 0, "NICE: Recebido": 1, "NICE: Atendido": 1, "NICE: Realizado": 11, "Total": 431 },
         "N6173067": { "Abertura RAL": 61, "RAL": 0, "REC": 0, "Remedy Móvel": 1260, "TOA: 1ª Int.": 3, "NICE: Recebido": 3, "NICE: Atendido": 3, "NICE: Realizado": 16, "Total": 1346 },
@@ -22,43 +36,45 @@ function buscarDados() {
         "F204763": { "Abertura RAL": 0, "RAL": 0, "REC": 0, "Remedy Móvel": 1793, "TOA: 1ª Int.": 5, "NICE: Recebido": 10, "NICE: Atendido": 9, "NICE: Realizado": 28, "Total": 1845 }
     };
 
-    let mediaEquipe = {};
-    let totalAnalistas = Object.keys(dados).length;
-    
-    for (let key in dados[Object.keys(dados)[0]]) {
-        mediaEquipe[key] = Object.values(dados).reduce((acc, curr) => acc + curr[key], 0) / totalAnalistas;
-    }
-    
     let analista = dados[login];
     if (!analista) {
         alert("Matrícula não encontrada.");
         return;
     }
-    
+
+    // Salvar última matrícula pesquisada
+    localStorage.setItem("ultimoLogin", login);
+
+    let mediaEquipe = {};
+    let totalAnalistas = Object.keys(dados).length;
+
+    for (let key in dados[Object.keys(dados)[0]]) {
+        mediaEquipe[key] = Object.values(dados).reduce((acc, curr) => acc + curr[key], 0) / totalAnalistas;
+    }
+
     tabelaDados.innerHTML = "";
     let pontosFracos = [];
     let pontosFortes = [];
-    
+
     for (let key in analista) {
-        let row = `<tr><td>${key}</td><td>${analista[key]}</td></tr>`;
-        tabelaDados.innerHTML += row;
-        
+        let rowClass = "";
         if (key !== "Total") {
             if (analista[key] < mediaEquipe[key]) {
+                rowClass = "ponto-fraco";
                 pontosFracos.push(key);
             } else if (analista[key] > mediaEquipe[key]) {
+                rowClass = "ponto-forte";
                 pontosFortes.push(key);
             }
         }
-    }
-    
-    if (analista.Total > mediaEquipe.Total) {
-        analise.innerText = `Parabéns! Você está acima da média da equipe. Seus pontos fortes: ${pontosFortes.join(", " )}. Continue assim!`;
-    } else if (analista.Total < mediaEquipe.Total) {
-        analise.innerText = `Atenção! Você está abaixo da média. Pontos a melhorar: ${pontosFracos.join(", " )}. Foco nesses indicadores!`;
-    } else {
-        analise.innerText = "Você está dentro da média da equipe. Continue monitorando seus indicadores!";
+        tabelaDados.innerHTML += `<tr class="${rowClass}"><td>${key}</td><td>${analista[key]}</td></tr>`;
     }
 
+    analise.innerText =
+        analista.Total > mediaEquipe.Total
+            ? `Parabéns! Você está acima da média da equipe. Seus pontos fortes: ${pontosFortes.join(", ")}. Continue assim!`
+            : `Atenção! Você está abaixo da média. Pontos a melhorar: ${pontosFracos.join(", ")}. Foco nesses indicadores!`;
+
     resultadoDiv.classList.remove("hidden");
+    resultadoDiv.classList.add("visivel");
 }
